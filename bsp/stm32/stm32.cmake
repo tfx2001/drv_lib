b9c8b5c48858aa7d5a85ddf74180fc7397e6850c
tfx2001 <2479727366@qq.com>
2021-02-03 16:58:03 +0800

    :building_construction: Add submodule STM32CubeF1

--- bsp/stm32/stm32.cmake
+++ bsp/stm32/stm32.cmake
@@ -1,3 +1,8 @@
-target_include_directories(drv_lib PUBLIC "bsp/stm32/")
+target_include_directories(drv_lib PUBLIC "bsp/stm32/"
+                                          "bsp/stm32/STM32CubeF1/Drivers/CMSIS/Core/Include"
+                                          "bsp/stm32/STM32CubeF1/Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+                                          "bsp/stm32/STM32CubeF1/Drivers/STM32F1xx_HAL_Driver/Inc"
+                                          "bsp/stm32/STM32CubeF1/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy")
 
-target_sources(drv_lib PRIVATE "bsp/stm32/drv_gpio.c" "bsp/stm32/drv_common.c")
+target_sources(drv_lib PRIVATE "bsp/stm32/drv_gpio.c"
+                               "bsp/stm32/drv_common.c")
